083faed10f36b77ba7741d4483b3e009101f588e
Крупин Евгений Евгеньевич <ekrupin@ecom.tech>
2026-04-13 15:20:06 +0300

    limit seed data to debug builds

--- app/src/main/java/ru/ekrupin/ivi/IviApplication.kt
+++ app/src/main/java/ru/ekrupin/ivi/IviApplication.kt
@@ -1,6 +1,7 @@
 package ru.ekrupin.ivi
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 import ru.ekrupin.ivi.data.local.seed.DatabaseSeeder
@@ -18,7 +19,14 @@ class IviApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ensureReminderNotificationChannel()
-        databaseSeeder.seedIfNeeded()
+
+        // Demo seed data is useful during local development, but release builds
+        // must start with an empty database for real usage.
+        val isDebuggableBuild = applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
+        if (isDebuggableBuild) {
+            databaseSeeder.seedIfNeeded()
+        }
+
         reminderScheduler.refreshAll()
     }
 }
